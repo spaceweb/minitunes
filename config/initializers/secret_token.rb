55a8f50f3f8927bb43b4e7379966c6218eafdadb
robinspace <robinspace2d@gmail.com>
2012-11-28 16:17:26 +0100

    Estado inicial del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minitunes::Application.config.secret_token = 'cbb906263c2e44c9f8aaf2393b17531fe6597a92276f0c0aa0dd60069565ce850e1dcc0af7a9aec8db87cdf73a1dd212a94ab8ad9c6632dab5220b19f2a6a6f3'
