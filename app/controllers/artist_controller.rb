665d3920141a6a3c802d9fb3115b115a0a6e1660
robinspace <robinspace2d@gmail.com>
2012-12-09 14:50:31 +0100

    Mejoras en la vista Artist, Applicacion, css

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,6 +1,11 @@
 class ArtistController < ApplicationController
 
   def show
+    @artist = Artist.find_by_name(params[:path])
+    if not @artist
+      @artist = Artist.new
+      flash[:notice] = "You search \"#{params[:path]}\" did not match anything on MiniTunes"
+    end
   end
 
 end
