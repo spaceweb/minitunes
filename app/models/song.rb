0765d996c52bc150b7019a62bcfd3f19c0239490
nereac <n.corteslomana@gmail.com>
2012-12-22 17:16:06 +0100

    añadidas restricciones de clave tabla songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ActiveRecord::Base
   attr_accessible :name, :track
-  has_many :contienes
+  has_many :contienes, :dependent => :destroy
   has_many :albums, :through => :contiene
+  validates_uniqueness_of :name
 end
  
