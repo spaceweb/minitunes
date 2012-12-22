1dff563f6651afe276c568b849c73c1db3511bce
nereac <n.corteslomana@gmail.com>
2012-12-22 17:10:31 +0100

    añadidas restricciones de clave tabla albums

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,8 +1,9 @@
 class Album < ActiveRecord::Base
   attr_accessible :name
-  has_many :participates
+  has_many :participates, :dependent => :destroy
   has_many :artists, :through => :participate
-  has_many :contienes
+  has_many :contienes, :dependent => :destroy
   has_many :songs, :through => :contiene
+  validates_uniqueness_os :name
 end
  
