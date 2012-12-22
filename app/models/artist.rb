fae71afbb7c8374371db3beb1a2a3bd88c3328be
nereac <n.corteslomana@gmail.com>
2012-12-22 17:08:28 +0100

    añadidas restricciones de clave tabla artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,12 +1,13 @@
 class Artist < ActiveRecord::Base
   attr_accessible :name, :description, :similar
   
-  has_many :participates
+  has_many :participates, :dependent => :destroy
   has_many :albums, :through => :participate
-  has_many :reviews
+  has_many :reviews, :dependent => :destroy
   has_many :tunegoers, :through => :review
-  has_many :tienes
+  has_many :tienes, :dependent => :destroy
   has_many :tunegoers, :through => :tiene
-  
+  validates_uniqueness_of :name
+
 end
  
