db23a7cb53236f97ae4174d54d79de62b02f32b4
robinspace <robinspace2d@gmail.com>
2012-12-25 21:30:22 +0100

    Modificamos el user model:
    	Ahora los atributos name, profile_name son accesibles
    	Corregimos errores según nuestra diagrama UML

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,20 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :profile_name
+
+  validates :name, presence: true
+  validates :profile_name, presence: true,
+                          uniqueness: true,
+                          format: {
+                            with: /a-zA-Z0-9_-/,
+                            message: "Must be formatted correctly."
+                          }
+  has_many :reviews
+  has_many :artists, :through => :review
+  has_many :friends, :dependent => :destroy
+  has_many :users, :through => :friend
+  has_many :adds, :dependent => :destroy
+  has_many :artists, :through => :add
   # attr_accessible :title, :body
 end
