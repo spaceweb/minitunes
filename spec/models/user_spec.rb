f15cb79860fa14efaf09539f3d93f465cc84eb51
robinspace <robinspace2d@gmail.com>
2012-12-25 22:47:06 +0100

    Corregimos error en user model y user_spec:
    profile_name debe ser una palabra sin espacios
    además corregimos spec user: require y la fixture

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,31 +1,35 @@
-require 'test_helper'
+require 'spec_helper'
 
-describe UserModel do
+describe User do
+  
+  fixtures :users
 
-  it "a user should enter a name" do
-    user = User.new
-    assert !user.save
-    assert !user.errors[:name].empty?
-  end
+  describe "UserModel" do
+    it "a user should enter a name" do
+      user = User.new
+      assert !user.save
+      assert !user.errors[:name].empty?
+    end
 
-  it "a user should enter a profile name" do
-    user = User.new
-    assert !user.save
-    assert !user.errors[:profile_name].empty?
-  end
+    it "a user should enter a profile name" do
+      user = User.new
+      assert !user.save
+      assert !user.errors[:profile_name].empty?
+    end
 
-  it "a user should a unique profile name" do
-    user = User.new
-    user.profile_name = users(:charlie).profile_name
-    assert !user.save
-    assert !user.errors[:profile_name].empty?
-  end
+    it "a user should a unique profile name" do
+      user = User.new
+      user.profile_name = users(:charlie).profile_name
+      assert !user.save
+      assert !user.errors[:profile_name].empty?
+    end
 
-  it "a user should have a profile name without spaces" do
-    user = User.new
-    user.profile_name = "My profile"
-    assert !user.save
-    assert !user.errors[:profile_name].empty?
-    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+    it "a user should have a profile name without spaces" do
+      user = User.new
+      user.profile_name = "My+profile"
+      assert !user.save
+      assert !user.errors[:profile_name].empty?
+      assert user.errors[:profile_name].include?("Must be formatted correctly.")
+    end
   end
 end
