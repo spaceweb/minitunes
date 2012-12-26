edb796fa6a030a79fef28ea73e1d8a30ff72d3e7
robinspace <robinspace2d@gmail.com>
2012-12-26 23:32:02 +0100

    Mejorar el estilo visual de los mensajes flash con bootstrap
    Para ello hemos declarado una función en app/helper llamada flash_class(type).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def flash_class(type)
+    case type
+    when :notice
+      "alert-success"
+    when :alert
+      "alert-error"
+    when :warning
+      "alert-error"
+    else
+      ""
+    end
+  end
 end
