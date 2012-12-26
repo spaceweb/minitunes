973936cfaac6146e6165d99ca021c9cda9c2ab87
jcamposs <jorge.camposserrano@gmail.com>
2012-12-26 09:28:29 +0100

    Cambiado has_many :contienes por :contains

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,7 @@ class Album < ActiveRecord::Base
   attr_accessible :name
   has_many :participates, :dependent => :destroy
   has_many :artists, :through => :participate
-  has_many :contienes, :dependent => :destroy
+  has_many :contains, :dependent => :destroy
   has_many :songs, :through => :contiene
   validates_uniqueness_of :name
 end
