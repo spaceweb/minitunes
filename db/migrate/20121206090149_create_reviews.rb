ce5babab4967f15f3f6225945c0596d911bbdaad
jcamposs <jorge.camposserrano@gmail.com>
2012-12-26 09:25:46 +0100

    Cambiado t.references :tunegoer a :user

--- db/migrate/20121206090149_create_reviews.rb
+++ db/migrate/20121206090149_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
       t.text :comments
-      t.references :tunegoer
+      t.references :user
       t.references :artist
     end
   end
