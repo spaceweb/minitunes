13ff7446f1f8af1279d99e1e6781202e61f5db7a
jcamposs <jorge.camposserrano@gmail.com>
2012-12-26 09:32:35 +0100

    Cambiado :tunegoer por :user en el modelo de review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,9 @@
 class Review < ActiveRecord::Base
   attr_accessible :review
-  validates_uniqueness_of :tunegoer, :scope => [:artist]
-  validates_presence_of :tunegoer
+  validates_uniqueness_of :user_id, :scope => [:artist_id]
+  validates_presence_of :user
   validates_presence_of :artist
   
-  belongs_to :tunegoer
+  belongs_to :user
   belongs_to :artist
 end
