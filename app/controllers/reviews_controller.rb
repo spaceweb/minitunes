e7ea5a0d7fce99b1e01acb9950458cf120595a68
jcamposs <jorge.camposserrano@gmail.com>
2012-12-29 19:31:49 +0100

    Añadida acción create en el controlador de Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,20 @@
 class ReviewsController < ApplicationController
-  
-  def new
-    
-  end
 
   def create
-    
+    if params[:commit] == "Cancel"
+      redirect_to artist_path
+    else
+      if signed_in?
+        @artist = Artist.find_by_name(params[:name])
+        @review = @artist.reviews.build(params[:review])
+        current_user.reviews << @review
+        @review.save!
+        flash[:notice] = 'Review successfully created.'
+        redirect_to artist_path
+      else
+        redirect_to login_path
+      end
+    end
   end
+
 end
