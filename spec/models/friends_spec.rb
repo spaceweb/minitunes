89aef16145cad8875606533995ff0637ca172302
robinspace <robinspace2d@gmail.com>
2012-12-30 00:05:14 +0100

    Corregimos test rspec para el modelo 'friendship'

--- spec/models/friends_spec.rb
+++ spec/models/friends_spec.rb
@@ -4,11 +4,8 @@ describe Friend do
   
   fixtures :users
 
-  should belong_to(:user)
-  should belong_to(:friend)
-
   it "creates a friendship" do
-    Friend.create user: users(:charlie), friend: users(:dean)
-    assert users(:charlie).friends.include?(users(:dean))
+    Friend.create user_id: users(:charlie).id, friend_id: users(:dean).id
+    users(:charlie).friends.include?(users(:dean).id)
   end
 end
