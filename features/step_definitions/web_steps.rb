639fb2d65ac55f4754d9288fd27d997becec4d0a
jcamposs <jorge.camposserrano@gmail.com>
2012-12-29 22:25:23 +0100

    Cambiados pasos de review_steps a web_steps

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -262,3 +262,16 @@ Then /^show me the page$/ do
   save_and_open_page
 end
 
+Given /the following Users exist/ do |user_table|
+  user_table.hashes.each do |user|
+    User.create!(user)
+  end
+end
+
+When /^I sign in as "(.*)\/(.*)"$/ do |email, password|
+  step %{I go to the Login page}
+  step %{I fill in "user_email" with "#{email}"}
+  step %{I fill in "user_password" with "#{password}"}
+  step %{I press "Sign in"}
+end
+
