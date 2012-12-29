86aaae76daf0cc9a57618d06615785c0b6c31412
jcamposs <jorge.camposserrano@gmail.com>
2012-12-29 18:44:29 +0100

    Corregidos errores en modelo Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,9 @@
 class Review < ActiveRecord::Base
-  attr_accessible :review
-  validates_uniqueness_of :user_id, :scope => [:artist_id]
-  validates_presence_of :user
-  validates_presence_of :artist
-  
+  attr_accessible :comments
+
   belongs_to :user
   belongs_to :artist
+
+  validates_presence_of :user
+  validates_presence_of :artist
 end
