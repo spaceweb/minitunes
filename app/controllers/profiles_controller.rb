4da6fd1c8f6d03d54d35f16e7fb7bd20b232868a
robinspace <robinspace2d@gmail.com>
2012-12-30 22:53:37 +0100

    Actializamos el controlador de profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,16 @@
 class ProfilesController < ApplicationController
+  before_filter :authenticate_user!
+
   def show
-  	if current_user
-	    @user = User.find_by_profile_name(params[:id])
-	    @artists = Artist.all
-	else
-		redirect_to :login
-	end
+    @user = User.find_by_profile_name(params[:id])
+    raise ActiveRecord::RecordNotFound if @user.nil?
+
+    if @user != current_user
+      @friend = @user
+    end
+
+    @artists = Artist.all
+  rescue ActiveRecord::RecordNotFound
+    render file: 'public/404', status: :not_found 
   end
 end
