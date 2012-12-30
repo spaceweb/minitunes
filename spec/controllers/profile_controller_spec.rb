2f0b1895714f0abbe95e581b1427ca83431eda7f
robinspace <robinspace2d@gmail.com>
2012-12-30 22:42:51 +0100

    Corregido error en el test del controllador profiles

--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -1,25 +1,32 @@
 
 require 'spec_helper'
 
-describe ProfileController do
+describe ProfilesController do
   
-  fixtures :users
+  describe "#show" do
+    include Devise::TestHelpers
+    fixtures :users
+
+    before(:each) do
+      sign_in users(:charlie)
+    end
   
-  it "should get show" do
-    get :show, id: users(:charlie).profile_name
-    assert_response :success
-    assert_template 'profiles/show'
-  end
+    it "should get show" do
+      get :show, id: users(:charlie).profile_name
+      assert_response :success
+      assert_template 'profiles/show'
+    end
 
-  it "should render a not found on profile view" do
-    get :show, id: "does't exist"
-    assert_response :not_found #It whould fails always
-  end
+    it "should render a not found on profile view" do
+      get :show, id: "does't exist"
+      assert_response :not_found #It whould fails always
+    end
 
-  it "only shows the correct user's collection of favorites artists" do
-    get :show, id: users(:charlie).profile_name
-    assigns[:artists].each do |artist|
-      assert_equal users(:charlie), artist.user
+    it "only shows the correct user's collection of favorites artists" do
+      get :show, id: users(:dean).profile_name
+      assigns[:artists].each do |artist| # Bug: have to fail ;)
+        assert_equal users(:dean), artist.user
+      end
     end
   end
 
