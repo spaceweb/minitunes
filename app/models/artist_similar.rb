82e828b01a862d70d6a4277f33300093d6223753
robinspace <robinspace2d@gmail.com>
2012-12-31 20:47:03 +0100

    Añadimos atributos artist_name, similar_name

--- app/models/artist_similar.rb
+++ app/models/artist_similar.rb
@@ -2,7 +2,8 @@ class ArtistSimilar < ActiveRecord::Base
   belongs_to :artist
   belongs_to :similar, class_name:'Artist', foreign_key: 'similar_id'
   validates :artist_id, presence: true
-  validates :name, presence: true
+  validates :artist_name, presence: true
+  validates :similar_name, presence: true
   # validates :similar_id, presence: true
-  attr_accessible :artist, :similar, :artist_id, :similar_id, :name
+  attr_accessible :artist, :similar, :artist_id, :similar_id, :artist_name, :similar_name
 end
--- db/migrate/20121231182050_create_artist_similars.rb
+++ db/migrate/20121231182050_create_artist_similars.rb
@@ -1,12 +1,11 @@
 class CreateArtistSimilars < ActiveRecord::Migration
   def change
     create_table :artist_similars do |t|
-      t.text :name
+      t.string :artist_name
+      t.string :similar_name
       t.integer :artist_id 
       t.integer :similar_id
       t.timestamps
     end
-
-    add_index :artist_similars, [:artist_id, :similar_id]
   end
 end
