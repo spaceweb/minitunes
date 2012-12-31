38ba872dde57168f1d05916a62b32d1d101f7224
robinspace <robinspace2d@gmail.com>
2012-12-31 20:48:24 +0100

    Has_many ArtistSimilar en el model Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,6 +7,8 @@ class Artist < ActiveRecord::Base
   has_many :users, :through => :review
   has_many :adds, :dependent => :destroy
   has_many :users, :through => :add
+  has_many :artist_similars
+  has_many :similars, through: :artist_similar
   validates_uniqueness_of :name
 
 end
