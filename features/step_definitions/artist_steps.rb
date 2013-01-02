16b986c3e1963442d2274e93119cf7251ccdcbf5
robinspace <robinspace2d@gmail.com>
2013-01-02 23:54:13 +0100

    Añadimos Step, dados dos artistas similares

--- features/step_definitions/artist_steps.rb
+++ features/step_definitions/artist_steps.rb
@@ -14,3 +14,15 @@ Then /I should see similar artist "(.+)"/ do |artist|
   print page.body
   page.should have_content(artist)
 end
+
+Given /"(.+)" and "(.+)" are similars/ do |one, two|
+  shakira = Artist.find_by_name(one)
+  paulina = Artist.find_by_name(two)
+  ArtistSimilar.create(artist_id: shakira.id, 
+                    similar_name: paulina.name, 
+                    artist_name: shakira.name)
+
+  ArtistSimilar.create(artist_id: paulina.id, 
+                    similar_name: shakira.name, 
+                    artist_name: paulina.name)
+end
