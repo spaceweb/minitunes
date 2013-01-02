04e8b226ce7b480251031f1a08b0015856e18a90
zalas87 <gonzalorm_1987@hotmail.com>
2013-01-02 15:28:50 +0100

    formato vista artists#show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,21 +1,27 @@
 class ArtistsController < ApplicationController
 
   def show
+    @albums = []
     @artist = Artist.find_by_name(params[:name])
-    if signed_in?
-      relations = Add.find_all_by_user_id(current_user.id)
-      relations.each do |relation|
-        if Artist.find_by_id(relation.artist_id).name == params[:name]
-          @add = true
+    participates = Participate.find_all_by_artist_id(@artist.id)
+    participates.each do |participate|
+      @albums << Album.find_by_id(participate.album_id)
+    end
+      
+    if @artist
+      if signed_in?
+        relations = Add.find_all_by_user_id(current_user.id)
+        relations.each do |relation|
+          if Artist.find_by_id(relation.artist_id).name == params[:name]
+            @add = true
+          end
         end
       end
-    end
-    if not @artist
-      @artist = Artist.new
-      flash[:notice] = "You search \"#{params[:name]}\" did not match anything on MiniTunes"
-    else
       @reviews = @artist.reviews
       @num_reviews = @reviews.length
+    else
+      @artist = Artist.new
+      flash[:notice] = "You search \"#{params[:name]}\" did not match anything on MiniTunes"
     end
   
   end
