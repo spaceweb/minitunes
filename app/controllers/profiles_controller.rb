f2a363a574bf82a06b3f143167a2388b47db1d2e
robinspace <robinspace2d@gmail.com>
2013-01-03 20:49:27 +0100

    Actualizamon mensaje flash cuando seguimos a un usuario

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,20 +6,20 @@ class ProfilesController < ApplicationController
     @user = User.find_by_profile_name(params[:id])
     raise ActiveRecord::RecordNotFound if @user.nil?
 
-    @friends = @user.friends
+    @following = @user.friends
     @artists = Array.new
     Add.where(user_id: @user).each do |add|
       @artists << Artist.find_by_id(add.artist_id)
     end
 
-    if @user != current_user and not @friends.include?(current_user)
-      @friend = @user # we can add as friends
+    if @user != current_user and not @following.include?(current_user)
+      @follow = @user # we can follow the user
     elsif @artists.length > 0 # User with similar taste
       others = Add.where(artist_id: @artists.sample.id)
       others.each_with_index do |other, i|
         break if i == 10
         similar = User.find_by_id(other.user_id)
-        if @user.id != other.user_id and not @friends.include?(similar)
+        if @user.id != other.user_id and not @following.include?(similar)
           @similars << similar
         end
       end
