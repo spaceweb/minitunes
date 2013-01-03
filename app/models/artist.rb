b624ecb9ec2004921f2f0605050ff10cd25d5378
jcamposs <jorge.camposserrano@gmail.com>
2013-01-03 13:49:46 +0100

    Añadida función para buscar en LastFM

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ActiveRecord::Base
   attr_accessible :name, :description
-  
+  require 'lastfm'
   has_many :participates, :dependent => :destroy
   has_many :albums, :through => :participate
   has_many :reviews, :dependent => :destroy
@@ -10,6 +10,19 @@ class Artist < ActiveRecord::Base
   has_many :artist_similars
   has_many :similars, through: :artist_similar
   validates_uniqueness_of :name
+
+  def self.api_key
+    '4eca38cd6745ccde896f9d2977916b15'
+  end
+
+  def self.api_secret
+    'is caa318fce04f21171ed7d4bf567e5ed4'
+  end
+
+  def self.find_in_lastfm(string)
+    lastfm = Lastfm.new(self.api_key, self.api_secret)
+    lastfm.artist.get_info(:artist => string)
+  end
 
 end
  
