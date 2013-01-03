4801ac99c38e5ccd0de90ac39a75ed7022efc237
robinspace <robinspace2d@gmail.com>
2013-01-03 19:04:44 +0100

    Actualizamos el controlador Profile
    La variable @similars tiene una lista de usuarios recomendados para el usuario

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,17 +2,27 @@ class ProfilesController < ApplicationController
   before_filter :authenticate_user!
 
   def show
+    @similars = Array.new
     @user = User.find_by_profile_name(params[:id])
     raise ActiveRecord::RecordNotFound if @user.nil?
-
-    if @user != current_user && !@user.friends.include?(current_user)
-      @friend = @user
-    end
 
     @friends = @user.friends
     @artists = Array.new
     Add.where(user_id: @user).each do |add|
       @artists << Artist.find_by_id(add.artist_id)
+    end
+
+    if @user != current_user and not @friends.include?(current_user)
+      @friend = @user # we can add as friends
+    else # User with similar taste
+      others = Add.where(artist_id: @artists.sample.id)
+      others.each_with_index do |other, i|
+        break if i == 10
+        similar = User.find_by_id(other.user_id)
+        if @user.id != other.user_id and not @friends.include?(similar)
+          @similars << similar
+        end
+      end
     end
    
   rescue ActiveRecord::RecordNotFound
