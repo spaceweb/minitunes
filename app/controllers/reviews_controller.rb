b884a264fa818cfe1186421e20ab060a0d9cdd7a
jcamposs <jorge.camposserrano@gmail.com>
2013-01-04 21:51:16 +0100

    Borrado botón Cancel del formulario de Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,15 @@
 class ReviewsController < ApplicationController
 
   def create
-    if params[:commit] == "Cancel"
+    if signed_in?
+      @artist = Artist.find_by_name(params[:name])
+      @review = @artist.reviews.build(params[:review])
+      current_user.reviews << @review
+      @review.save!
+      flash[:notice] = 'Review successfully created.'
       redirect_to artist_path
     else
-      if signed_in?
-        @artist = Artist.find_by_name(params[:name])
-        @review = @artist.reviews.build(params[:review])
-        current_user.reviews << @review
-        @review.save!
-        flash[:notice] = 'Review successfully created.'
-        redirect_to artist_path
-      else
-        redirect_to login_path
-      end
+      redirect_to login_path
     end
   end
 
