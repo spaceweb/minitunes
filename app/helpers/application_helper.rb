b83bdc8daec31c23ee781dcbdd6a476e11a17280
robinspace <robinspace2d@gmail.com>
2013-01-05 22:12:37 +0100

    Función que extrae elementos repetidos de un array

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,11 @@ module ApplicationHelper
   def uri(string)
     URI.parse(URI.encode(string))
   end
+
+  def repeated(array)
+    # Extract repeated elements in a Array
+    counts = Hash.new(0)
+    array.each{|val|counts[val]+=1}
+    counts.reject{|val, count|count==1}.keys
+  end
 end
