b540c534ce5ea58e2e0bcec1a2941e364fad41c7
jcamposs <jorge.camposserrano@gmail.com>
2013-01-09 18:19:26 +0100

    Modificado modelo Song

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,4 @@ class Song < ActiveRecord::Base
   attr_accessible :name, :track, :duration
   has_many :contains, :dependent => :destroy
   has_many :albums, :through => :contain
-  validates_uniqueness_of :name
 end
- 
