4c8d8d736e69c0e1cf3ca191360b92664f56f580
jcamposs <jorge.camposserrano@gmail.com>
2013-01-12 17:51:24 +0100

    Añadido javascript player

---
+++ app/assets/javascripts/player.js
@@ -0,0 +1,25 @@
+PLAYERAjax = {
+    setup: function() {
+        $('<div id="videoSong"></div>').
+            hide().
+            appendTo($('body'));
+        $('#songs a').click(PLAYERAjax.getVideoUrl);
+    },
+    getVideoUrl: function() {
+        $.ajax({type: 'GET',
+                url: $(this).attr('href'),
+                timeout: 5000,
+                success: PLAYERAjax.showVideoSong,
+                error: function() { alert('Error!'); }
+               });
+        return(false);
+    },
+    showVideoSong: function(data) {
+        $('#videoSong').
+            html(data).
+            css({'left': 603, 'top': 148}).
+            show();
+        return(false);
+    },
+}
+$(PLAYERAjax.setup);
