c99d9f9d15dcdaf54bf8937d7de69c2ab446a4fd
jcamposs <jorge.camposserrano@gmail.com>
2013-01-12 16:24:56 +0100

    Añadido atributo link en el modelo de song

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
-  attr_accessible :name, :track, :duration
+  attr_accessible :name, :track, :duration, :link
   has_many :contains, :dependent => :destroy
   has_many :albums, :through => :contain
 end
