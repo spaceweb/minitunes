f79a349fd4c4ec948d3dc77fd9df4a7bbe299b6e
robinspace <robinspace2d@gmail.com>
2013-01-13 16:08:50 +0100

    Añadimos feature Following para la vista following

--- features/step_definitions/users_steps.rb
+++ features/step_definitions/users_steps.rb
@@ -5,3 +5,9 @@ Given /(.+) is a follower of (.+)$/ do |follower_name, user_name|
     user = User.find_by_name(user_name)
     follower.friendships.create!(friend: user)
 end
+
+Given /^me, (.+), I am following to (.+)$/ do |user_name, follower_name|
+    follower = User.find_by_name(follower_name)
+    user = User.find_by_name(user_name)
+    user.friendships.create!(friend: follower)
+end
