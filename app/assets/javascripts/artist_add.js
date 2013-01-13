d5ce9b470a785ccf04db4633ba746d5c0b1c9bfb
robinspace <robinspace2d@gmail.com>
2013-01-13 14:41:38 +0100

    Añadimos Ajax a los botones Add Artist, y Remove Artist

---
+++ app/assets/javascripts/artist_add.js
@@ -0,0 +1,45 @@
+
+window.addBtn;
+window.removeBtn;
+AddArtist = {
+  setupAdd: function() {
+    $('.btn-add').one("click", function(event) {
+      event.preventDefault();
+      addBtn = $(this);
+      removeBtn = $('.btn-remove[id='+addBtn.attr('id')+']');
+      $.ajax({
+        url: '/user_artist/create',
+        data: { id: addBtn.attr('id') },
+        type: 'POST',
+        success: AddArtist.toggleAdd
+      });
+      return(false);
+    })
+  },
+  setupRemove: function() {
+    $('.btn-remove').one("click", function(event) {
+      event.preventDefault();
+      removeBtn = $(this);
+      addBtn = $('.btn-add[id='+removeBtn.attr('id')+']');
+      $.ajax({
+        url: '/user_artist/destroy',
+        data: { id: removeBtn.attr('id') },
+        type: 'DELETE',
+        success: AddArtist.toggleRemove
+      });
+      return(false);
+    })
+  },
+  toggleAdd: function(data) {
+    addBtn.hide();
+    removeBtn.show();
+    return(false);
+  },
+  toggleRemove: function(data) {
+    addBtn.show();
+    removeBtn.hide();
+    return(false);
+  }
+}
+$(AddArtist.setupAdd);
+$(AddArtist.setupRemove);
