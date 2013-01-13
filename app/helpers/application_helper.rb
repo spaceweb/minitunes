bc6ad77218069398aeb7214d89a1410379490629
robinspace <robinspace2d@gmail.com>
2013-01-13 13:19:53 +0100

    Añadimos la función que extrae elementos repetidos de un array al ProfileHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,10 +17,4 @@ module ApplicationHelper
     URI.parse(URI.encode(string))
   end
 
-  def repeated(array)
-    # Extract repeated elements in a Array
-    counts = Hash.new(0)
-    array.each{|val|counts[val]+=1}
-    counts.reject{|val, count|count==1}.keys
-  end
 end
--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,8 @@
 module ProfilesHelper
+  def repeated(array)
+    # Extract repeated elements in a Array
+    counts = Hash.new(0)
+    array.each{|val|counts[val]+=1}
+    counts.reject{|val, count|count==1}.keys
+  end
 end
