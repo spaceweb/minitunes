c708a01cdd8a643461123fea1a4c9cc03a9daaa2
zalas87 <gonzalorm_1987@hotmail.com>
2013-01-13 12:53:35 +0100

    filter.js ahora se llama album_filter.js

--- app/assets/javascripts/album_filter.js
+++ app/assets/javascripts/album_filter.js
@@ -1,23 +1,23 @@
-FILTER = {
+ALBUM_FILTER = {
     setup: function() {
       if ($('#albums >tbody >tr').length > 20) {
         $('<br>' + '<input type="button" id="filter_albums" value="See less" class= "btn btn-primary"/>'
-          ).insertAfter('#albums').click(FILTER.filter_albums);
+          ).insertAfter('#albums').click(ALBUM_FILTER.filter_albums);
       }
     },
     filter_albums: function () {
-      $('#albums tbody tr').each(FILTER.seeLess);
+      $('#albums tbody tr').each(ALBUM_FILTER.seeLess);
     },
     seeLess: function () {
       if (! /^(\d|1\d|20)\.$/i.test($(this).find('td:nth-child(1)').text())) {
         $(this).hide();
-        $('#filter_albums').replaceWith('<input type="button" id="filter_albums" value="See more" class= "btn btn-primary" onclick="FILTER.seeMore()"/>');
+        $('#filter_albums').replaceWith('<input type="button" id="filter_albums" value="See more" class= "btn btn-primary" onclick="ALBUM_FILTER.seeMore()"/>');
       }
     },
     seeMore: function () {
       $('#albums tbody tr').show();
       $('#filter_albums').remove();
-      $(FILTER.setup);
+      $(ALBUM_FILTER.setup);
     }
 }
-$(FILTER.setup); 
+$(ALBUM_FILTER.setup); 
