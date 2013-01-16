4b377977bfed6c5c9021480959b1ccc4973b2ae4
jcamposs <jorge.camposserrano@gmail.com>
2013-01-16 15:46:10 +0100

    Añadida funcionalidad reconocimiento de voz para añadir comentarios

---
+++ app/assets/javascripts/x-webkit-speech.js
@@ -0,0 +1,5 @@
+function transcribe(words) {
+  document.getElementById("textareareviews").value = words;
+  document.getElementById("inputspeech").value = "";
+  document.getElementById("textareareviews").focus();
+}
