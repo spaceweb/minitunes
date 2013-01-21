053f3f9b64019498d7fe222eb467b83b6016bac9
zalas87 <gonzalorm_1987@hotmail.com>
2013-01-22 00:05:15 +0100

    solucionado problema url de los similares

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -17,7 +17,7 @@ class ArtistsController < ApplicationController
           end
         end
       end
-      @artist_search = Artist.find_in_lastfm(@artist.name)
+      @artist_search = Artist.find_in_lastfm(@artist.name.gsub('##$','.').gsub('##@','/'))
       @artist_image = @artist_search["image"][4]["content"]
       @reviews = @artist.reviews
       @num_reviews = @reviews.length
@@ -36,18 +36,18 @@ class ArtistsController < ApplicationController
     artist_name = params[:search].split.collect!{|word| word.capitalize}.join(' ')
     @artist = Artist.find_by_name(artist_name) || Artist.find_by_name(artist_name.upcase)
     if not @artist
-      @artist_search = Artist.find_in_lastfm(params[:search])
+      @artist_search = Artist.find_in_lastfm(params[:search].gsub('##$','.').gsub('##@','/'))
       if @artist_search and @artist_search["bio"]["summary"] != {}
         begin
           @artist = createArtist(@artist_search)
         rescue
-          redirect_to artist_path(@artist_search["name"])
+          redirect_to artist_path(@artist_search["name"].gsub('.','##$').gsub('/','##@'))
           return
         end
         if @artist
           createSimilar(@artist, @artist_search["similar"]["artist"])
 
-          @albumssearch = Artist.find_top_albums_in_lastfm(params[:search])
+          @albumssearch = Artist.find_top_albums_in_lastfm(params[:search].gsub('##$','.').gsub('##@','/'))
           createAlbums(@albumssearch, @artist) if @albumssearch
         end
       end
@@ -76,7 +76,7 @@ class ArtistsController < ApplicationController
 
   protected
   def createArtist(artist_search)
-    artist = {:name => artist_search["name"], :description => artist_search["bio"]["summary"]}
+    artist = {:name => artist_search["name"].gsub('.','##$').gsub('/','##@'), :description => artist_search["bio"]["summary"]}
     artist[:description] = artist[:description].sub(/<(.*)>/,'')
     @artist = Artist.create!(artist) if artist[:description] != ''
     return @artist
