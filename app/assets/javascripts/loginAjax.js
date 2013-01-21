09b97754fdd12c6caf8ab7b0a2e826dbbc038a1a
robinspace <robinspace2d@gmail.com>
2013-01-21 20:14:47 +0100

    Corregido error en el fallo de authent¡ficación (Login)
    Mostramos un mensaje flash en el diálogo indicando el motivo del fallo

--- app/assets/javascripts/loginAjax.js
+++ app/assets/javascripts/loginAjax.js
@@ -15,23 +15,20 @@ SubmitLogin = {
                 e.preventDefault();
             }
             var user_info = $(this).serializeObject();
-            console.log("Route:::", $(this).attr('action'), user_info);
             $.ajax({
                 type: "POST",
                 url: $(this).attr('action'),
                 data: user_info,
                 success: function(data){
-                    console.log("The Devise Response: " + JSON.stringify(data));
                     jdata = jQuery(data);
                     logindata = jdata.find(".nav.pull-right");
-                    console.log(logindata);
                     $('.nav.pull-right').html(logindata);
                     $('#loginModal').modal('toggle');
                 },
                 error: function(data){
-                    console.log(location.pathname);
-                    console.log("Error Response: " + JSON.stringify(data));
-                    //alert("The Devise Response: " + JSON.stringify(json));
+                    $(".alert.alert-error").remove();
+                    jdata = '<div class="alert alert-error">'+data.responseText+'</div>'
+                    $('.input.email.optional').before(jdata);
                 }
             });
         });
