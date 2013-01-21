b46ff1c63c7c6f5520a2ec9803a2a668b50fd65c
zalas87 <gonzalorm_1987@hotmail.com>
2013-01-21 22:57:12 +0100

    Solucion al problema de la vista al crear un commentario

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -15,6 +15,13 @@ Comments = {
             content = $(".reviews-content").children().remove();
             $(".reviews-content").append(data);
             console.log(data);
+            $('#reviewspaginate').pajinate({
+              items_per_page : 10,
+              num_page_links_to_display : 5,
+              abort_on_small_lists: true,
+              item_container_id : '.alt_content',
+              nav_panel_id : '.alt_page_navigation'
+            });
         }else {
             $(".link-Login").click();
         }
